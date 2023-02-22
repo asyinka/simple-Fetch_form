25dd9f829126727a0c3a6d0db3e88b0ca36ce97c
Yinka <user.asyinka@gmail.com>
2023-02-22 12:15:08 +0100

    this is my project on the calling APIs in forms

--- country.js
+++ country.js
@@ -15,38 +15,101 @@ const option = {
 };
 
 let submitButton = document.querySelector(".profile-card__button");
+let countrySelect = document.querySelector("#country");
+let countryOption = `<option value="">select your country of choice</option>`;
+let stateSelect = document.querySelector("#state");
 
-// let dataURL = 'https://countries-states-cities-dataset.p.rapidapi.com/list-countries';
-let finalURL = 'https://referential.p.rapidapi.com/v1/city?ip=128.218.229.26';
+
+let finalURL = 'https://referential.p.rapidapi.com/v1/country?fields=iso_a2';
 
 
-submitButton.addEventListener("click", handleSubmitButton);
+// submitButton.addEventListener("click", handleSubmitButton);
 
-async function handleSubmitButton() {
+// async function handleSubmitButton() {
+
+//     console.log("clickedd")
     
-    try {
-        let result = await fetch('https://referential.p.rapidapi.com/v1/city?ip=128.218.229.26', option);
-        let actualResult = await result.json();
-        console.log(actualResult);
-    } catch (error) {
-        errMessage.innerHTML = `
-            Error alert, ${error.message};
-        `
-    }
+// }
+
+// handleCountryOnchange();
+handleCountryOnchange();
+countrySelect.addEventListener('change', handleFillStateOption);
+
+async function handleFillStateOption(event){
+   const countryCode = event.currentTarget.value;
+   console.log(countryCode);
+
+   const statesList = await fetchState(countryCode);
+
+   handleStateList(statesList);
 
 }
 
+async function fetchCountry() {
+    try {
+        let result = await fetch(finalURL, option);
 
-// const option = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '61599e639cmsh766add475f0c45ap10729ajsn71c5b14020c6',
-// 		'X-RapidAPI-Host': 'referential.p.rapidapi.com'
-// 	}
-// };
+        if (result.ok) {
+            let actualResult = await result.json();
+            // console.log(actualResult)
+            return actualResult;
+        } else {
+            throw new Error(result.statusText != "" ? result.statusText : result.status);
+        }
+        
+        // let countryList = await actualResult;
+    } catch (error) {
 
-// fetch('https://referential.p.rapidapi.com/v1/city?ip=128.218.229.26', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
+        errMessage.innerHTML = `
+            Error alert, ${error.message};
+        `
+        console.log(error);
+    }
+}
 
+async function handleCountryOnchange() {
+    let countryList = await fetchCountry();
+    
+    // console.log(countryList);
+
+    for (countryArr of countryList) {
+        countryOption = countryOption + `<option value="${countryArr.iso_a2}">${countryArr.value}</option>`;
+    }
+
+    countrySelect.innerHTML = countryOption;
+}
+
+async function fetchState(countryCode) {
+    try {
+        let result = await fetch(`https://referential.p.rapidapi.com/v1/state?iso_a2=${countryCode}`, option);
+        
+        if (result.ok) {
+            let actualResult = await result.json();
+
+            return actualResult;
+        } else {
+            throw new Error(result.statusText != "" ? result.statusText : result.status);
+        }
+        
+        // let countryList = await actualResult;
+    } catch (error) {
+
+        errMessage.innerHTML = `
+            Error alert, ${error.message};
+        `
+        console.log(error);
+    }
+}
+
+async function handleStateList(statesList) {
+    // statesList = await fetchState(countryCode);
+    let optionHtml = '<option value="">select state of choice</option>';
+
+    // console.log(countryList);
+
+    for (stateArr of statesList) {
+        optionHtml = optionHtml + `<option value="${stateArr.key}">${stateArr.value}</option>`;
+    }
+
+    stateSelect.innerHTML = optionHtml;
+}
